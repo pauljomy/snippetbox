53cfceec8deec3c6bf9efefa67c941c053e86575
Paul Jomy <pauljomy@users.noreply.github.com>
2026-03-26 08:00:58 +0530

    working on sql

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -5,6 +5,8 @@ import (
 	"log/slog"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql" // New import
 )
 
 type application struct {
@@ -14,9 +16,18 @@ type application struct {
 func main() {
 
 	addr := flag.String("addr", ":4000", "HTTP network address")
+	dsn := flag.String("mysql", "web:pass@snippetbox?parseTime=true", "MySQL data source name")
 	flag.Parse()
 
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	db, err := openDB(*dsn)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+
+	defer db.Close()
 
 	app := &application{logger: logger}
 
